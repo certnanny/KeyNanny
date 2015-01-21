ec0689f7bd6e480af95fa85da7bfdebac9f36fb4
Martin Bartosch <m.bartosch@cynops.de>
2015-01-21 18:12:55 +0100

    allow package build with relocated perl libraries
    usage:
    PERLLIBDIR=/opt/keynanny/lib/perl5 make package

--- packaging/Linux/make_Linux_package.sh
+++ packaging/Linux/make_Linux_package.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+# creates KeyNanny spec file and builds package.
+# target location of KeyNanny library files (default: vendor perl path)
+# can be overridden via PERLLIBDIR
+#
+# example:
+# PERLLIBDIR=/opt/keynanny/lib/perl5 make_Linux_package.sh
 
 echo "Creating package..."
 version=$(head -n 1 VERSION)
-perllibdir=$(perl -e 'use Config; print $Config{installvendorlib}')
-echo "Perl libs go to $perllibdir ..."
-sed "s/VERSIONINFO/$version/" < packaging/Linux/keynanny.spec.in | sed "s'PERLLIBPATH'$perllibdir'" > packaging/Linux/keynanny.spec
+[ -z "$PERLLIBDIR" ] && PERLLIBDIR=$(perl -e 'use Config; print $Config{installvendorlib}')
+echo "Perl libs go to $PERLLIBDIR ..."
+sed "s/VERSIONINFO/$version/" < packaging/Linux/keynanny.spec.in | sed "s'PERLLIBPATH'$PERLLIBDIR'" > packaging/Linux/keynanny.spec
 mkdir keynanny-$version
 tar cf - bin doc README.md lib examples | (cd keynanny-$version; tar xf -)
 tar -czf $HOME/rpmbuild/SOURCES/keynanny-$version.tar.gz keynanny-$version
