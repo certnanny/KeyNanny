6964a7cc45738bf008b6e0aa1d476ee88e87b369
gknocke <gideonknocke@gmail.com>
2015-03-03 14:16:28 +0100

    the pythonic way

--- lib/keyNanny.py
+++ lib/keyNanny.py
@@ -1,131 +1,100 @@
 '''
 #usage:
 from keyNanny import KeyNanny
-kn = KeyNanny(SOCKETFILE='/path/to/socket', LOGGER = '/path/to/log')
+kn = KeyNanny(socketfile='/path/to/socket', logger = '/path/to/log')
 kn.set("foo", "bar")
 value = kn.get("foo")
 print value['DATA']
+#kn.ping()
 #kn.link("str1", "str2")
 #kn.list()
 '''
-
 import logging
-import sys
-import os, stat
 import socket
 
 class KeyNanny:
 	def __init__(self, **kwargs):
-		try:
-			log = kwargs.pop('LOGGER')
-			logging.basicConfig(filename=log, level=logging.DEBUG)
-		except:
-			logging.warning('init: log file not specified')
-			logging.basicConfig(level=logging.DEBUG)	
-		try:
-			self.SOCKETFILE = kwargs.pop('SOCKETFILE')
-		except: 
-			logging.error('init: No socketfile specified')
-			sys.exit('init: No socketfile specified')		
-		self.init_socket()
+		socket = None
+		socketfile = None
+		for key in kwargs:
+			if key == 'logger':
+				logfile = kwargs[key]	
+			elif key == 'socketfile':
+				socketfile = kwargs[key]
+			elif key == 'socket':
+				socket = kwargs[key]
+		if socket:
+			if socketfile:
+				raise Exception("socket and socketfile are mutually exclusive")
+			self._socket = socket
+		elif socketfile:
+			self._init_socket(socketfile)
+		else:
+			raise Exception("please specify socket OR socketfile")
+		logging.basicConfig(filename=logfile, level=logging.DEBUG)
 		
-	def init_socket(self):
-		try:
-			mode=os.stat(self.SOCKETFILE).st_mode
-		except:
-			logging.error('init_socket: %s does not exist' % self.SOCKETFILE)
-			sys.exit('init_socket: %s does not exist' % self.SOCKETFILE)
-		if not stat.S_ISSOCK(mode):
-			logging.error('init_socket: %s is not a socket file', self.SOCKETFILE)
-			sys.exit('init_socket: %s is not a socket file' % self.SOCKETFILE)
-		if not stat.S_IRUSR & mode:
-			logging.error('init_socket: %s is not readable', self.SOCKETFILE)
-			sys.exit('init_socket: %s is not readable' % self.SOCKETFILE)
-		if not stat.S_IWUSR & mode:
-			logging.error('init_socket: %s is not writable', self.SOCKETFILE)
-			sys.exit('init_socket: %s is not writable' % self.SOCKETFILE)
-		
-		try:
-			self.SOCKET = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-			self.SOCKET.connect(self.SOCKETFILE)
-		except socket.error as err:
-			self.SOCKET.close()
-			self.SOCKET = None
-			logging.error('init_socket: Cannot connect with server, error: %s' %(err))
-		
-	def get(self, *arg):
-		self.init_socket()
-		self.send_command({'CMD': 'get', 'ARG': arg})
-		return self.receive_response()
+	def _init_socket(self, socketfile):
+		self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		self._socket.connect(socketfile)
+		logging.info('init_socket: Succesfully created socket %s' % socketfile)
 	
-	def send_command(self, args):
-		if not args['CMD']:
-			logging.error("send_command: No command specified")
-			sys.exit("send_command: No command specified")
-		self.send({'DATA': " ".join([args['CMD']] + list(args['ARG']))})
+	def _send_command(self, cmd, args = None, binary=None):
+		if isinstance(args, str):
+			args = [args]
+		if args is None:
+			args = []
+		self._send(" ".join([cmd] + args), binary)
 		
-	def send(self, args):
-		if not 'BINARY' in args:
-			args['DATA'] += '\r\n'
-		try:
-			self.SOCKET.sendall(args['DATA'])
-		except socket.error:
-			logging.error("send: sending through socket failed")
-			sys.exit("send: sending through socket failed")
-
-	def receive_response(self):
-		return self.receive()
+	def _send(self, args, binary=None):
+		if binary is None:
+			args += '\r\n'
+		self._socket.sendall(args)
 		
-	def receive(self):
-		code = self.read_line()
+	def _receive(self):
+		code = self._read_line()
 		result = {}
 		if not ' ' in code:
-			result['STATUS'] = code
+			result['STATUS'] = code.rstrip()
 			result['MESSAGE'] = ''
 			return result
 		else:
 			status, message = code.split(' ', 1)
-			if not status == 'OK':
+			if status != 'OK':
 				logging.error("receive: Server responded with error")
-				result['STATUS'] = status
-				result['MESSAGE'] = message
+				result['STATUS'] = status.rstrip()
+				result['MESSAGE'] = message.rstrip()
 			else:
 				l = int(message)
 				logging.info("receive: Succesfully received answer from server")
-				result['STATUS'] = status
+				result['STATUS'] = status.rstrip()
 				result['MESSAGE'] = message.rstrip()
-				result['DATA'] = self.read_line(l)
+				result['DATA'] = self._read_line(l)
 			return result
 
-	def read_line(self, *num):
+	def _read_line(self, num=None):
+		"read a line from a socket or num chars if given"
 		chars = []
 		if num:
-			try:
-				a = self.SOCKET.recv(*num)
-			except socket.error, e:
-				logging.warning("%s" % e)
-				return ""
+			a = self._socket.recv(num)
 			return a.rstrip()
 		while True:
-			try:
-				a = self.SOCKET.recv(1)
-			except socket.error, e:
-				logging.warning("%s" % e)
-				return ""
+			a = self._socket.recv(1)
 			if a == "\n" or a == "":
 				return "".join(chars)
 			chars.append(a)   
+	
+	def get(self, arg):
+		self._send_command('get', arg)
+		return self._receive()
 			  
 	def set(self, key, value):
-		self.init_socket()
-		self.send_command({'CMD': 'set', 'ARG': [key, str(len(value))]})
-		self.send({'DATA': value, 'BINARY': 1})
-		return self.receive_response()
+		self._send_command('set', [key, str(len(value))])
+		self._send(value, True)
+		return self._receive()
 		
 	def list(self):
-		self.init_socket()
-		self.send_command({'CMD': 'list', 'ARG': []})
-		result = self.receive_response()
+		self._send_command('list')
+		result = self._receive()
 		if result['STATUS'] == 'OK':
 			result['KEYS'] = result['DATA'].split()
 		else:
@@ -133,11 +102,9 @@ class KeyNanny:
 		return result
 		
 	def ping(self):
-		self.init_socket()
-		self.send_command({'CMD': 'ping', 'ARG': []})
-		return self.receive_response()
+		self._send_command('ping')
+		return self._receive()
 
 	def link(self, dest, origin):
-		self.init_socket()
-		self.send_command({'CMD': 'link', 'ARG': [dest, origin]})
-		return self.receive_response()
+		self._send_command('link', [dest, origin])
+		return self._receive()
